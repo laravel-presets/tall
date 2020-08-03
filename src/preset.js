f297325206f62d0a98830f973240a03c98d716e4
Enzo Innocenzi <enzo@innocenzi.dev>
2020-08-03 17:21:22 +0200

    refactor: update to fluent API

--- src/preset.js
+++ src/preset.js
@@ -1,96 +1,63 @@
-const { Preset, Prompt, flags, Log, Color } = require('use-preset');
-const spawn = require('cross-spawn');
+const { Preset } = require('use-preset');
 
-module.exports = Preset.make({
-	name: 'Laravel TALL',
+// prettier-ignore
+module.exports = Preset.make('Laravel TALL')
+	.option('auth', false)
 
-	parse: () => ({
-		flags: {
-			auth: flags.boolean({ default: false }),
-		},
-	}),
-
-	actions: () => [
-		{
-			type: 'edit-json',
-			file: 'package.json',
-			merge: {
-				devDependencies: {
-					'@tailwindcss/ui': '^0.1',
-					'laravel-mix-tailwind': '^0.1.0',
-					alpinejs: '^2.0',
-					tailwindcss: '^1.4',
-				},
+	.editJson('package.json')
+		.title('Add Tailwind and Alpine')
+		.merge({
+			devDependencies: {
+				'@tailwindcss/ui': '^0.1',
+				alpinejs: '^2',
+				tailwindcss: '^1',
+				'postcss-nested': '^4',
+				'postcss-import': '^12',
 			},
-			delete: ['devDependencies.lodash', 'devDependencies.axios'],
-		},
-		{
-			type: 'edit-json',
-			file: 'composer.json',
-			merge: {
-				require: {
-					'livewire/livewire': '^1.2',
-				},
-			},
-		},
-		{
-			type: 'delete',
-			directories: 'resources/sass',
-		},
-		{
-			type: 'copy',
-			directories: 'default',
-			strategy: 'override',
-		},
-		{
-			type: 'copy',
-			if: context => Boolean(context.flags.auth),
-			directories: 'auth',
-			strategy: 'override',
-		},
-		{
-			type: 'edit',
-			files: [
-				'app/Providers/RouteServiceProvider.php',
-				'app/Http/Middleware/RedirectIfAuthenticated.php',
-			],
-			replace: [
-				{
-					search: `public const HOME = '/home';`,
-					with: `public const HOME = '/';`,
-				},
-				{
-					search: `RouteServiceProvider::HOME`,
-					with: `route('home')`,
-				},
-			],
-		},
-	],
-	after: async context => {
-		if (context.flags.auth) {
-			Log.info(`Authentication has been scaffolded.`);
-		}
+		})
+		.delete(['devDependencies.lodash', 'devDependencies.axios'])
+		.chain()
 
-		// Propose to install dependencies
-		if (await Prompt.confirm('Do you want to install NPM dependencies now?')) {
-			await context.installDependencies();
-			Log.success('Successfully installed dependencies.');
-		}
-
-		// prettier-ignore
-		if (await Prompt.confirm(`Do you want to run ${Color.blue('composer update')} to install Livewire?`)) {
-			const { status, output } = spawn.sync('composer', ['update']);
-			if (status !== 0) {
-				output
-					.join(' ')
-					.split('\n')
-					.filter(Boolean)
-					.forEach(message => {
-						Log.fatal(message.trim())
-					})
-			} else {
-				Log.success('Updated composer dependencies.')
+	.editJson('composer.json')
+		.title('Add Livewire')
+		.merge({
+			require: {
+				'livewire/livewire': '^1.2',
 			}
-		}
-	},
-});
+		})
+		.chain()
+		
+	.delete()
+		.title('Delete SASS')
+		.directories('resources/sass')
+		.chain()
+
+	.copyDirectory('default')
+		.to('/')
+		.title('Copy templates')
+		.whenConflict('override')
+		.chain()
+
+	.copyDirectory('auth')
+		.to('/')
+		.title('Scaffold authentication')
+		.if(({ flags }) => Boolean(flags.auth))
+		.whenConflict('override')
+		.chain()
+
+	.edit(['app/Providers/RouteServiceProvider.php', 'app/Http/Middleware/RedirectIfAuthenticated.php'])
+		.title('Update route configuration')
+		.replace(`public const HOME = '/home';`).with(`public const HOME = '/';`)
+		.replace(`public const HOME = '/home';`).with(`public const HOME = '/';`)
+		.replace(`$namespace = 'App\\Http\\Controllers'`).with(`$namespace = ''`)
+		.chain()
+
+	.installDependencies()
+		.for('node')
+		.title('Install node dependencies')
+		.chain()
+
+	.updateDependencies()
+		.for('php')
+		.title('Install PHP dependencies')
+		.chain();
